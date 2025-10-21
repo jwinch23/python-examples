d2cb5547cf5fe07d0a0c0286ffbe0622b6fc446a
Josh Winchester <jwinchester23@gmail.com>
2025-10-20 20:35:11 -0500

    Update encoder-cli
    Add some error handling for when a user does not provide a valid input string.

--- examples/encoder/src/encoder/encoder-cli.py
+++ examples/encoder/src/encoder/encoder-cli.py
@@ -2,5 +2,8 @@ import sys
 from encoder import run_length_encode
 
 if __name__ == "__main__":
-    input_str = sys.argv[1]
-    print(run_length_encode(input_str))
+    if len(sys.argv) > 1 and sys.argv[1].strip():
+        input_str = sys.argv[1]
+        print(run_length_encode(input_str))
+    else: 
+        print("Invalid: Please provide a string to analyze.")
